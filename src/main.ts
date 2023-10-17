5ddee02124914d22d19aa55bad9cedb3ee6e92fa
Bryan Louie Martinez <bry.martinez@gmail.com>
2023-10-17 22:18:10 +0800

    IIFE

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,51 @@
 import { DataSource } from './data/datasource';
 import { RelationshipObject } from './relationships/relationship';
 
-const datasource = DataSource.getInstance();
+(async () => {
+  const datasource = DataSource.getInstance();
 
-datasource.addMember({
-  name: 'King Arthur',
-  gender: 'M',
-  mother: undefined,
-  father: undefined,
-  siblings: undefined,
-  children: [],
-  spouse: {
-    name: 'Queen Margret',
-    gender: 'F',
-  },
-});
-
-datasource.addMember({
-  name: 'Queen Margret',
-  gender: 'F',
-  mother: undefined,
-  father: undefined,
-  siblings: undefined,
-  children: [],
-  spouse: {
+  datasource.addMember({
     name: 'King Arthur',
     gender: 'M',
-  },
-});
+    mother: undefined,
+    father: undefined,
+    siblings: undefined,
+    children: [],
+    spouse: {
+      name: 'Queen Margret',
+      gender: 'F',
+    },
+  });
 
-datasource.addChild('Queen Margret', 'Bill', 'M');
-datasource.addChild('Queen Margret', 'Charlie', 'M');
-datasource.addChild('Queen Margret', 'Percy', 'M');
-datasource.addSpouse('Bill', 'Flora', 'F');
-datasource.addChild('Bill', 'Victoire', 'F');
-datasource.addChild('Bill', 'Dominique', 'F');
-datasource.addChild('Bill', 'Louis', 'M');
-datasource.addSpouse('Victoire', 'Ted', 'M');
-datasource.addChild('Victoire', 'Remus', 'M');
+  datasource.addMember({
+    name: 'Queen Margret',
+    gender: 'F',
+    mother: undefined,
+    father: undefined,
+    siblings: undefined,
+    children: [],
+    spouse: {
+      name: 'King Arthur',
+      gender: 'M',
+    },
+  });
 
-// const command = 'GET_RELATIONSHIP Bill Mother';
+  datasource.addChild('Queen Margret', 'Bill', 'M');
+  datasource.addChild('Queen Margret', 'Charlie', 'M');
+  datasource.addChild('Queen Margret', 'Percy', 'M');
+  datasource.addSpouse('Bill', 'Flora', 'F');
+  datasource.addChild('Bill', 'Victoire', 'F');
+  datasource.addChild('Bill', 'Dominique', 'F');
+  datasource.addChild('Bill', 'Louis', 'M');
+  datasource.addSpouse('Victoire', 'Ted', 'M');
+  datasource.addChild('Victoire', 'Remus', 'M');
 
-const rel = 'mother';
+  // const command = 'GET_RELATIONSHIP Bill Mother';
 
-console.log(
-  RelationshipObject[rel]?.get('Remus') || new Error('Relationship not found.'),
-);
+  const rel = 'mother';
+
+  console.log(
+    RelationshipObject[rel]?.get('Remus') ||
+      new Error('Relationship not found.'),
+  );
+})();
